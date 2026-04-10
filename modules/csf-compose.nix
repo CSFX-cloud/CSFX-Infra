22bb4f14340fdf714cc0ae62a3e4afe9e5e68763
CodeMaster4711 <cedi27@t-online.de>
2026-04-10 21:11:38 +0200

    fix: use lowercase substituteAll argument names

--- modules/csf-compose.nix
+++ modules/csf-compose.nix
@@ -5,65 +5,15 @@ let
   ghcrOrg = "csfx-cloud";
   imageRef = svc: "ghcr.io/${ghcrOrg}/csf-ce-${svc}@${v.images.${svc}.digest}";
 
-  composeFile = pkgs.writeText "csf-docker-compose.yml" ''
-    services:
-      etcd:
-        image: quay.io/coreos/etcd:v3.5.12
-        restart: unless-stopped
-        command:
-          - etcd
-          - --data-dir=/data
-          - --listen-client-urls=http://0.0.0.0:2379
-          - --advertise-client-urls=http://localhost:2379
-        volumes:
-          - etcd-data:/data
-        network_mode: host
-
-      csf-api-gateway:
-        image: ${imageRef "api-gateway"}
-        restart: unless-stopped
-        env_file: /etc/csf/cp.env
-        network_mode: host
-        depends_on: [etcd]
-
-      csf-registry:
-        image: ${imageRef "registry"}
-        restart: unless-stopped
-        env_file: /etc/csf/cp.env
-        network_mode: host
-        depends_on: [etcd]
-
-      csf-scheduler:
-        image: ${imageRef "scheduler"}
-        restart: unless-stopped
-        env_file: /etc/csf/cp.env
-        network_mode: host
-        depends_on: [etcd]
-
-      csf-volume-manager:
-        image: ${imageRef "volume-manager"}
-        restart: unless-stopped
-        env_file: /etc/csf/cp.env
-        network_mode: host
-        depends_on: [etcd]
-
-      csf-failover-controller:
-        image: ${imageRef "failover-controller"}
-        restart: unless-stopped
-        env_file: /etc/csf/cp.env
-        network_mode: host
-        depends_on: [etcd]
-
-      csf-sdn-controller:
-        image: ${imageRef "sdn-controller"}
-        restart: unless-stopped
-        env_file: /etc/csf/cp.env
-        network_mode: host
-        depends_on: [etcd]
-
-    volumes:
-      etcd-data:
-  '';
+  composeFile = pkgs.substituteAll {
+    src = ./docker-compose.yml;
+    image_api_gateway         = imageRef "api-gateway";
+    image_registry            = imageRef "registry";
+    image_scheduler           = imageRef "scheduler";
+    image_volume_manager      = imageRef "volume-manager";
+    image_failover_controller = imageRef "failover-controller";
+    image_sdn_controller      = imageRef "sdn-controller";
+  };
 in
 {
   config = {
