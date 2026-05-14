d171762b5e88f0bc5458d2d28c8bc830c451cec0
CodeMaster4711 <cedi27@t-online.de>
2026-05-14 21:03:40 +0200

    revert(installer): restore direct ISO boot, drop installer approach

--- flake.nix
+++ flake.nix
@@ -12,61 +12,10 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
 
-    mkSystem = { system, modules, extraSpecialArgs ? {} }: nixpkgs.lib.nixosSystem {
+    mkSystem = { system, modules }: nixpkgs.lib.nixosSystem {
       inherit system;
-      specialArgs = { inherit versions; } // extraSpecialArgs;
+      specialArgs = { inherit versions; };
       modules = modules;
-    };
-
-    serverModules = { hostPlatform, nixosConfig }: [
-      self.nixosModules.csfx-agent
-      self.nixosModules.csfx-cp
-      self.nixosModules.csfx-setup
-      self.nixosModules.csfx-binary-cache
-      self.nixosModules.update-units
-      ({ lib, ... }: {
-        system.stateVersion = "24.11";
-        boot.loader.systemd-boot.enable = true;
-        boot.loader.efi.canTouchEfiVariables = true;
-        boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
-        fileSystems."/" = { device = "/dev/disk/by-label/nixos"; fsType = "ext4"; };
-        fileSystems."/boot" = { device = "/dev/disk/by-label/boot"; fsType = "vfat"; options = [ "umask=0077" ]; };
-        swapDevices = [];
-        nixpkgs.hostPlatform = hostPlatform;
-        networking.hostName = "csfx-node";
-        networking.useDHCP = true;
-        networking.firewall.enable = true;
-        networking.firewall.allowedTCPPorts = [ 22 8000 ];
-        time.timeZone = "UTC";
-        services.openssh.enable = true;
-        services.openssh.settings.PermitRootLogin = "prohibit-password";
-        services.openssh.settings.PasswordAuthentication = false;
-        services.csfx-agent.enable = true;
-        services.csfx-agent.gatewayUrl = "http://localhost:8000";
-        services.csfx-cp.enable = true;
-        services.csfx-cp.dbUrl = "";
-        services.csfx-cp.etcdEndpoints = "http://localhost:2379";
-        services.csfx-cp.updater.infraRepoMirrorUrl = "https://github.com/CSFX-cloud/CSFX-Infra.git";
-        services.csfx-cp.updater.infraRepoGithub = "CSFX-cloud/CSFX-Infra";
-        services.csfx-cp.updater.infraRepoBranch = "main";
-        services.csfx-setup.enable = true;
-        services.csfx-setup.dataPart = "/dev/sda3";
-        services.csfx-binary-cache.enable = true;
-        services.csfx-update-units.enable = true;
-        services.csfx-update-units.nixCacheUrl = "http://localhost:5000";
-        services.csfx-update-units.nixCachePublicKey = "";
-        services.csfx-update-units.nixosConfig = nixosConfig;
-      })
-    ];
-
-    serverSystem = mkSystem {
-      system = "x86_64-linux";
-      modules = serverModules { hostPlatform = "x86_64-linux"; nixosConfig = "csfx-node"; };
-    };
-
-    serverSystemArm64 = mkSystem {
-      system = "aarch64-linux";
-      modules = serverModules { hostPlatform = "aarch64-linux"; nixosConfig = "csfx-node-arm64"; };
     };
   in {
     checks.${system}.pre-commit = pre-commit-hooks.lib.${system}.run {
@@ -88,20 +37,36 @@
       csfx-cp           = import ./modules/csfx-cp.nix;
       csfx-binary-cache = import ./modules/csfx-binary-cache.nix;
       csfx-setup        = import ./modules/csfx-setup.nix;
-      csfx-installer    = import ./modules/csfx-installer.nix;
       update-units      = import ./modules/update-units.nix;
     };
 
     nixosConfigurations = {
       csfx-node = mkSystem {
         system = "x86_64-linux";
-        extraSpecialArgs = { serverToplevel = serverSystem.config.system.build.toplevel; };
         modules = [
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          self.nixosModules.csfx-installer
+          self.nixosModules.csfx-agent
+          self.nixosModules.csfx-cp
+          self.nixosModules.csfx-setup
+          self.nixosModules.csfx-binary-cache
+          self.nixosModules.update-units
           ({ lib, ... }: {
             system.stateVersion = "24.11";
-            services.csfx-installer.enable = true;
+            services.csfx-agent.enable = true;
+            services.csfx-agent.gatewayUrl = "http://localhost:8000";
+            services.csfx-cp.enable = true;
+            services.csfx-cp.dbUrl = "";
+            services.csfx-cp.etcdEndpoints = "http://localhost:2379";
+            services.csfx-cp.updater.infraRepoMirrorUrl = "https://github.com/CSFX-cloud/CSFX-Infra.git";
+            services.csfx-cp.updater.infraRepoGithub = "CSFX-cloud/CSFX-Infra";
+            services.csfx-cp.updater.infraRepoBranch = "main";
+            services.csfx-setup.enable = true;
+            services.csfx-setup.dataPart = "/dev/sda3";
+            services.csfx-binary-cache.enable = true;
+            services.csfx-update-units.enable = true;
+            services.csfx-update-units.nixCacheUrl = "http://localhost:5000";
+            services.csfx-update-units.nixCachePublicKey = "";
+            services.csfx-update-units.nixosConfig = "csfx-node";
             image.fileName = lib.mkForce "csfx-node.iso";
           })
         ];
@@ -109,13 +74,30 @@
 
       csfx-node-arm64 = mkSystem {
         system = "aarch64-linux";
-        extraSpecialArgs = { serverToplevel = serverSystemArm64.config.system.build.toplevel; };
         modules = [
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          self.nixosModules.csfx-installer
+          self.nixosModules.csfx-agent
+          self.nixosModules.csfx-cp
+          self.nixosModules.csfx-setup
+          self.nixosModules.csfx-binary-cache
+          self.nixosModules.update-units
           ({ lib, ... }: {
             system.stateVersion = "24.11";
-            services.csfx-installer.enable = true;
+            services.csfx-agent.enable = true;
+            services.csfx-agent.gatewayUrl = "http://localhost:8000";
+            services.csfx-cp.enable = true;
+            services.csfx-cp.dbUrl = "";
+            services.csfx-cp.etcdEndpoints = "http://localhost:2379";
+            services.csfx-cp.updater.infraRepoMirrorUrl = "https://github.com/CSFX-cloud/CSFX-Infra.git";
+            services.csfx-cp.updater.infraRepoGithub = "CSFX-cloud/CSFX-Infra";
+            services.csfx-cp.updater.infraRepoBranch = "main";
+            services.csfx-setup.enable = true;
+            services.csfx-setup.dataPart = "/dev/sda3";
+            services.csfx-binary-cache.enable = true;
+            services.csfx-update-units.enable = true;
+            services.csfx-update-units.nixCacheUrl = "http://localhost:5000";
+            services.csfx-update-units.nixCachePublicKey = "";
+            services.csfx-update-units.nixosConfig = "csfx-node-arm64";
             image.fileName = lib.mkForce "csfx-node-arm64.iso";
           })
         ];
